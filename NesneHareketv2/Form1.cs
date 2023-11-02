2eb5d2040c10cf625e97de1f307f75134483022d
Emir <emirkan.ulker9@gmail.com>
2023-11-03 00:57:56 +0300

    v2.3

---
+++ NesneHareketv2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NesneHareketv2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+
+            if(e.KeyCode == Keys.D)
+            {
+                x = x + 5;
+            }
+
+            if(e.KeyCode == Keys.A)
+            {
+                x = x - 5;
+            }
+
+            if(e.KeyCode == Keys.W)
+            {
+                y = y - 5;
+            }
+
+            if(e.KeyCode== Keys.S)
+            {
+                y = y + 5;
+            }
+
+            if(pictureBox1.Right >= label1.Left)
+            {
+                label1.BackColor = Color.Red;
+            }
+
+            if(pictureBox1.Left <= label2.Right)
+            {
+                label2.BackColor = Color.Cyan;
+            }
+
+            if(pictureBox1.Bottom >= label3.Top)
+            {
+                label3.BackColor = Color.Purple;
+            }
+
+            if(pictureBox1.Top <= label4.Bottom)
+            {
+                label4.BackColor = Color.Yellow;
+            }
+
+            pictureBox1.Location = new Point(x, y);
+        }
+    }
+}
